76cf79ee2382cf2cf8797f3cf2aba63608436460
RuPaul <105512611+RuPaul23@users.noreply.github.com>
2023-08-28 23:18:39 +0530

    Add files via upload

---
+++ FarmaaCo/src/pages/HomePage.js
@@ -0,0 +1,126 @@
+import "./HomePage.css";
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <img className="vector-icon1" alt="" src="/vector1.svg" />
+      <img className="vector-icon2" alt="" src="/vector2.svg" />
+      <div className="farmaaco-is-a">
+        FarmaaCo is a cutting-edge business website that aims to revolutionize
+        the agricultural industry. By leveraging real-time weather data, we
+        provide timely financial support to farmers in need. Our innovative
+        platform ensures that funds are disbursed automatically when adverse
+        weather conditions impact crops, enabling farmers to recover and thrive.
+      </div>
+      <div className="home-page-child" />
+      <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+      <div className="contact-us">Contact Us</div>
+      <div className="about-us">About Us</div>
+      <div className="sign-up">Sign Up</div>
+      <div className="our-services">Our Services</div>
+      <div className="our-services1">Our Services</div>
+      <div className="about-us1">About Us</div>
+      <div className="contact-us1">Contact Us</div>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="get-started">Get Started</div>
+      </div>
+      <div className="rectangle-group">
+        <div className="group-item" />
+        <div className="more-info">More Info</div>
+        <img
+          className="arrow-point-to-right"
+          alt=""
+          src="/arrow-point-to-right@2x.png"
+        />
+      </div>
+      <div className="rectangle-container">
+        <div className="group-item" />
+        <div className="more-info">More Info</div>
+        <img
+          className="arrow-point-to-right"
+          alt=""
+          src="/arrow-point-to-right@2x.png"
+        />
+      </div>
+      <div className="group-div">
+        <div className="group-item" />
+        <div className="more-info">More Info</div>
+        <img
+          className="arrow-point-to-right"
+          alt=""
+          src="/arrow-point-to-right@2x.png"
+        />
+      </div>
+      <img className="image-7-icon" alt="" src="/image-7@2x.png" />
+      <div className="image-5-parent">
+        <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+        <b className="flexible-loan-amounts">Flexible Loan Amounts</b>
+        <div className="we-offer-a-container">
+          <p className="we-offer-a">
+            We offer a range of loan amounts tailored to the unique needs of
+          </p>
+          <p className="we-offer-a">
+            farmers to ensure they get the support they need.
+          </p>
+        </div>
+      </div>
+      <div className="image-6-parent">
+        <img className="image-6-icon" alt="" src="/image-6@2x.png" />
+        <b className="weather-based-eligibility">Weather-Based Eligibility</b>
+        <div className="our-farmer-loan-container">
+          <p className="we-offer-a">
+            Our farmer loan approval process is based on real-time weather
+          </p>
+          <p className="we-offer-a">
+            data to ensure farmers benefit most from the loan.
+          </p>
+        </div>
+      </div>
+      <img className="vector-icon3" alt="" src="/vector3.svg" />
+      <div className="image-4-parent">
+        <img className="image-4-icon" alt="" src="/image-4@2x.png" />
+        <div className="our-efficient-loan">
+          Our efficient loan application process and quick approval turnaround
+          ensure farmers receive prompt support.
+        </div>
+        <b className="fast-and-easy">Fast and Easy Approval</b>
+      </div>
+      <img
+        className="farmaaco-logo-2-1"
+        alt=""
+        src="/farmaacologo-2-1@2x.png"
+      />
+      <div className="discover-a-new">
+        Discover a new era of agri-innovation with Farmaaco. Our cutting-edge
+        platform is designed to revolutionize the way farmers and lenders
+        interact, collaborate, and thrive in the ever-evolving landscape of
+        agriculture.
+      </div>
+      <div className="welcome-to-farmaaco">Welcome To FarmaaCo</div>
+      <div className="home-parent">
+        <div className="home">Home</div>
+        <img className="group-child1" alt="" src="/vector-1.svg" />
+      </div>
+      <div className="fill-out-the">
+        Fill out the form below to get in touch with us. We are here to answer
+        any questions you may have and provide the best solution for your needs.
+      </div>
+      <div className="home-page-item" />
+      <div className="home-page-inner" />
+      <div className="home-page-child1" />
+      <div className="home-page-child2" />
+      <div className="name">Name</div>
+      <div className="message">Message</div>
+      <div className="send">Send</div>
+      <div className="e-mail">E-Mail</div>
+      <div className="home-page-child3" />
+      <div className="farmaaco">FarmaaCo</div>
+      <img className="facebook-icon" alt="" src="/facebook@2x.png" />
+      <img className="twitter-icon" alt="" src="/twitter@2x.png" />
+      <img className="instagram-icon" alt="" src="/instagram@2x.png" />
+    </div>
+  );
+};
+
+export default HomePage;
